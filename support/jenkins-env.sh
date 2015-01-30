9d6b10abab0847379a1aea7d9eb24d31c3596b6b
Mark Petrovic <mark.petrovic@xoom.com>
2015-01-29 19:30:46 -0800

    improve support script

--- support/jenkins-env.sh
+++ support/jenkins-env.sh
@@ -4,17 +4,26 @@
 
 set -vux
 
+function cleanup {
+   rm -f prog.pl
+}
+trap cleanup EXIT
+
 JOB_NAME=theservice-release
 CURRENT_RELEASE_VERSION=1.1
 DEVELOPMENT_VERSION=1.2-SNAPSHOT
 
-COMPONENT_NAME=$(perl -e 'if ( $ARGV[0] =~ /(.*)$ARGV[1]$/ ) { print $1 } else { exit(1) }' -- $JOB_NAME -release)
+cat <<"F" > prog.pl
+if ( $ARGV[0] =~ /(.*)$ARGV[1]$/ ) { print $1 } else { exit(1) }
+F
+
+COMPONENT_NAME=$(perl -- prog.pl $JOB_NAME -release)
 if [ $? != 0 ]; then
 	echo "JOB_NAME does not contain the suffix -release.  Cannot determine Jira component name. Exiting."
 	exit 1
 fi
 
-NEXT_VERSION=$(perl -e 'if ( $ARGV[0] =~ /(.*)$ARGV[1]$/ ) { print $1 } else { exit(1) }' -- $DEVELOPMENT_VERSION -SNAPSHOT)
+NEXT_VERSION=$(perl -- prog.pl $DEVELOPMENT_VERSION -SNAPSHOT)
 if [ $? != 0 ]; then
 	echo "DEVELOPMENT_VERSION does not contain the suffix -SNAPSHOT  Cannot determine component next-version name. Exiting."
 	exit 1
@@ -28,5 +37,4 @@ echo ./jirarelease-linux-amd64 \
 	-component-name $COMPONENT_NAME \
 	-release-version-name $CURRENT_RELEASE_VERSION \
 	-next-version-name $NEXT_VERSION
-
 
